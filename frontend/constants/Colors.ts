89b5e2e626c3aa5584f2d18a49fedd311c7904af
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2025-03-16 02:58:22 +0300

    Update Colors.ts

--- frontend/constants/Colors.ts
+++ frontend/constants/Colors.ts
@@ -1,8 +1,9 @@
 export const Colors = {
   primary: "#FFFFFF",
   secondary: "#A0ADBA",
-  main: "#54B7E2",
+  main: "#0EA5E9",
   headerText: "#49535C",
-  footerButton: "#0EA5E9",
   backgroundScreen: "#F4F2F2",
+  border: "#CCD7ED",
+  inputInactiveText: "#7C7C7C"
 };
