8240a4f61d7dfdef794a98633026c7a2dcaf4363
Hrushenka <alinauglova@mail.ru>
2025-03-16 04:32:01 +0300

    Изменения в соответствии с футером и хеадером + список первым отображается

--- frontend/components/DoctorMain/RegistrationForm.tsx
+++ frontend/components/DoctorMain/RegistrationForm.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet} from 'react-native';
 import { Colors } from "@/constants/Colors";
 
 interface RegistrationData {
