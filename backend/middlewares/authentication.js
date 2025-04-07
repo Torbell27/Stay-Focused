808e95d7ff5b665f4fd4d0e98670ed5a89808cb7
Mooncat3 <mark.frizen730@gmail.com>
2025-04-07 14:46:18 +0300

    Добавление текста ошибок для middleware

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -4,10 +4,10 @@ const { verify } = pkg;
 
 const authenticate = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.status(401).json({ detail: "Authorization required" });
 
   verify(token, process.env.SESSION_SECRET_KEY, (err, user) => {
-    if (err) return res.sendStatus(401);
+    if (err) return res.status(401).json({ detail: "Authorization required" });
     req.userId = user.id;
     req.userRole = user.role;
     next();
--- backend/middlewares/checkUserRole.js
+++ backend/middlewares/checkUserRole.js
@@ -3,13 +3,14 @@ import pkg from "jsonwebtoken";
 const { verify } = pkg;
 
 const checkUserRole = (role) => (req, res, next) => {
-    const token = req.headers["authorization"]?.split(" ")[1];
-    if (!token) return res.sendStatus(401);
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) return res.status(401).json({ detail: "Authorization required" });
 
-    verify(token, process.env.SESSION_SECRET_KEY, (err, user) => {
-      if (err || parseInt(user.role) !== role) return res.sendStatus(401);
-      next();
-    });
-}
+  verify(token, process.env.SESSION_SECRET_KEY, (err, user) => {
+    if (err || parseInt(user.role) !== role)
+      return res.status(401).json({ detail: "Authorization required" });
+    next();
+  });
+};
 
 export { checkUserRole };
