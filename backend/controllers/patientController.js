2cec104e611a60690c9aeba36e451e6d812b01b9
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2025-04-07 15:16:57 +0300

    Update patientController.js

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -18,3 +18,20 @@ export const get = async (req, res) => {
     return res.status(500).json({ detail: "Server error" });
   }
 };
+
+/* export const getActivity = async (req, res) => {
+  const patientId = req.userId;
+
+  try {
+    await pool.query(`SET app.user_uuid = '${patientId}'`);
+    const request = await pool.query(`SELECT activity FROM users_pub`);
+    if (request.rows.length > 0) {
+      return res.status(200).json(request.rows);
+    } else {
+      return res.status(204).json({ detail: "Activity does not exist" });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ detail: "Server error" });
+  }
+}; */
