596ee1a3cc15039ff403b8d9fc095b08a48491a6
Mooncat3 <mark.frizen730@gmail.com>
2025-04-21 18:39:37 +0300

    Исправление вохможности переполнения часов у пациента из-за часового пояса

--- backend/utilities/userStatToLocale.js
+++ backend/utilities/userStatToLocale.js
@@ -1,24 +1,75 @@
-const userStatToLocale = (userStatistics) => {
-  const result = [...userStatistics];
+import generateLocaleTimestamp from "./generateLocaleTimestamp.js";
 
-  userStatistics.forEach((record, index) => {
-    const { date, data } = record;
+const userStatToLocale = (userStatistics, startDate, endDate) => {
+  let result = [...userStatistics];
+
+  const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+
+  const searchDate = (date) => {
+    return result.findIndex(
+      (element) => element.date.toISOString() === date.toISOString()
+    );
+  };
+
+  userStatistics.forEach(({ date, data }, index) => {
     const newTimeStat = {};
     Object.entries(data.time_stat).forEach(([k, v]) => {
+      const objectToSave = {};
+      objectToSave[k] = {
+        ...v,
+        timestamp_start: generateLocaleTimestamp(
+          v.timestamp_start,
+          v.patient_timezone
+        ),
+      };
+
       const newDate = new Date(
         (v.timestamp_start - v.patient_timezone * 60) * 1000
       );
-      const newTimestamp =
-        newDate.getUTCHours() * 3600 +
-        newDate.getUTCMinutes() * 60 +
-        newDate.getUTCSeconds();
+      const offset = newDate.getTime();
 
-      newTimeStat[k] = { ...v, timestamp_start: newTimestamp };
+      if (Math.abs(offset) >= oneDayInMilliseconds) {
+        const dateWithOffset = new Date(date);
+        dateWithOffset.setTime(dateWithOffset.getTime() + offset);
+        const cleanDate = new Date(dateWithOffset.toISOString().split("T")[0]);
+        cleanDate.setUTCHours(21);
+
+        delete data.time_stat[k];
+
+        const searchIndex = searchDate(cleanDate);
+        if (searchIndex === -1)
+          result.push({ date: cleanDate, data: { time_stat: objectToSave } });
+        else
+          result[searchIndex] = {
+            date: cleanDate,
+            data: {
+              ...data,
+              time_stat: {
+                ...result[searchIndex].data.time_stat,
+                ...objectToSave,
+              },
+            },
+          };
+      } else newTimeStat[k] = objectToSave[k];
     });
 
-    result[index] = { date, data: { ...data, time_stat: newTimeStat } };
+    result[index] = {
+      date,
+      data: {
+        ...data,
+        time_stat: { ...result[index].data.time_stat, ...newTimeStat },
+      },
+    };
   });
-  return result;
+
+  [...result].forEach((record) => {
+    if (Object.keys(record.data.time_stat).length === 0)
+      result = result.filter((e) => e.date !== record.date);
+  });
+
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  return result.filter((e) => e.date >= start && e.date <= end);
 };
 
 export default userStatToLocale;
